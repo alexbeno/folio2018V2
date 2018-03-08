9e6229a9061dba7ed8fe1cd0e6d05c9a24f11b61
alexbeno <alexbenoliel@gmail.com>
2018-03-08 17:44:06 +0100

    🚀 add enter project animation

--- src/components/project/bmw/_bmw.js
+++ src/components/project/bmw/_bmw.js
@@ -8,9 +8,15 @@
 export default {
   name: 'bmw',
   methods: {
+    init() {
+      const page = document.querySelector('.project');
 
+      page.classList.add('project--active');
+    },
   },
   mounted: function () {
-
+    setTimeout(() => {
+      this.init();
+    }, 100);
   },
 };
--- src/components/project/bmw/bmw.vue
+++ src/components/project/bmw/bmw.vue
@@ -15,6 +15,66 @@
       <h1 class="information__titleContent information__titleContent--bmw">BMW.</h1>
     </div>
 
+    <!-- description -->
+    <div class="information__describ__container">
+
+      <!-- description texte -->
+      <div class="information__describ">
+        <p class="information__describ__content">Alii nullo quaerente vultus severitate adsimulata patrimonia sua in inmensum extollunt, cultorum ut puta feracium multiplicantes annuos fructus, quae a primo ad ultimum solem se abunde iactitant possidere, ignorantes profecto maiores suos.</p>
+      </div>
+
+      <!-- site information -->
+      <div class="information__site">
+        <div class="information__siteItem">
+          <div class="information__siteItem__title">
+            <p class="information__siteItem__titleContent">WHY</p>
+          </div>
+          <div class="information__siteItem__text">
+            <p class="information__siteItem__textContent">School Project</p>
+          </div>
+        </div>
+        <div class="information__siteItem">
+          <div class="information__siteItem__title">
+            <p class="information__siteItem__titleContent">DATE</p>
+          </div>
+          <div class="information__siteItem__text">
+            <p class="information__siteItem__textContent">2017</p>
+          </div>
+        </div>
+        <div class="information__siteItem">
+          <div class="information__siteItem__title">
+            <p class="information__siteItem__titleContent">ROLE</p>
+          </div>
+          <div class="information__siteItem__text">
+            <p class="information__siteItem__textContent">Front / back develloper</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- launch site -->
+      <a class="information__discover" href="#" target="°blank">
+          <div class="information__disoverBorder"></div>
+          <div class="information__disoverContent">
+            <div class="information__disoverText">
+                <p class="information__disoverText__static">Launch <span>Site</span></p>
+              </div>
+            <div class="information__disoverText--hover">
+                <p class="information__disoverText__static--hover">Let's <span>Go</span></p>
+              </div>
+          </div>
+        </a>
+    </div>
+
+    <!-- details -->
+    <div class="details">
+      <div class="details__container">
+
+          <div class="details__container__video">
+              <video class="details__container__video__content" src="@/../static/video/bmw/config.mp4" autoplay loop muted></video>
+          </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
