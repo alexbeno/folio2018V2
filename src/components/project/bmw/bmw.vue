1246c857536ea35aff16ec39401ec25bd03a13eb
alexbeno <alexbenoliel@gmail.com>
2018-03-08 18:51:25 +0100

    💄 add will change and change translate by transform3d

--- src/components/project/bmw/bmw.vue
+++ src/components/project/bmw/bmw.vue
@@ -68,10 +68,13 @@
     <!-- details -->
     <div class="details">
       <div class="details__container">
+        <div class="details__container__video details__container__video--1">
+          <video class="details__container__video__content" src="@/../static/video/bmw/config.mp4" autoplay loop muted></video>
+        </div>
 
-          <div class="details__container__video">
-              <video class="details__container__video__content" src="@/../static/video/bmw/config.mp4" autoplay loop muted></video>
-          </div>
+        <div class="details__container__video details__container__video--2">
+          <video class="details__container__video__content" src="@/../static/video/bmw/config.mp4" autoplay loop muted></video>
+        </div>
       </div>
     </div>
 
