19001e193ea3698fc62b696b383210e7ae102724
alexbeno <alexbenoliel@gmail.com>
2018-03-10 11:48:30 +0100

    🚀 deploying go to up for close event

--- src/components/project/bmw/_bmw.js
+++ src/components/project/bmw/_bmw.js
@@ -47,12 +47,69 @@ export default {
     closes() {
       const close = document.querySelector('.headerNav__link');
       const page = document.querySelector('.project');
+      const details = document.querySelector('.details');
+
       close.addEventListener('click', () => {
-        page.classList.add('project--leave');
-        setTimeout(() => {
-          router.push({ path: '/' });
-        }, 2000);
+        if (details.scrollTop > 0) {
+          this.goUp(300);
+          setTimeout(() => {
+            page.classList.add('project--leave');
+            setTimeout(() => {
+              router.push({ path: '/' });
+            }, 2000);
+          }, 400);
+        } else {
+          page.classList.add('project--leave');
+          setTimeout(() => {
+            router.push({ path: '/' });
+          }, 2000);
+        }
       });
+    },
+    /**
+     * @function closes
+     * @description close scrolling project
+     */
+    goUp: function (dur) {
+      // select DOM element
+      const page = document.querySelector('.details');
+
+      // init variable
+
+      const to = 0;
+      const duration = dur;
+      const start = page.scrollTop;
+      const change = to - start;
+      let currentTime = 0;
+      const increment = 20;
+
+      // animate fonction
+
+      const animateScroll = () => {
+        currentTime += increment;
+        const val = this.easeInOutQuad(currentTime, start, change, duration);
+        page.scrollTop = val;
+        if (currentTime < duration) {
+          setTimeout(animateScroll, increment);
+        }
+      };
+      animateScroll();
+    },
+
+    /**
+     * @function easeInOutQuad
+     * @description ease transition for closing scroll
+     */
+    easeInOutQuad: (tP, bP, cP, dP) => {
+      let t = tP;
+      const b = bP;
+      const c = cP;
+      const d = dP;
+
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t -= 1;
+      return -c / 2 * (t *(t - 2) - 1) + b;
     },
   },
   mounted: function () {
